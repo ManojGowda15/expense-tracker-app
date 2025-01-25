251f0bf7e2929fb52728dd29c193a63fd882ab9d
ManojGowda15 <manojv13579@gmail.com>
2025-01-25 13:36:42 +0530

    first commit and Designed UI for Intro Page

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,117 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import Button from "@/components/Button";
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        {/* Login Button and Image */}
+        <View>
+          {/* Sign In */}
+          <TouchableOpacity style={styles.loginButton}>
+            <Typo fontWeight={"500"}>Sign In</Typo>
+          </TouchableOpacity>
+
+          {/* Welcome Image */}
+          <Animated.Image
+            entering={FadeIn.duration(1000).springify().damping(12)}
+            source={require("../../assets/images/welcome.png")}
+            resizeMode="contain"
+            style={styles.welcomeImage}
+          />
+        </View>
+
+        {/* Footer */}
+        <View style={styles.footer}>
+          <Animated.View
+            entering={FadeInDown.duration(1000).springify().damping(12)}
+            style={{ alignItems: "center" }}
+          >
+            <Typo size={30} fontWeight={"700"}>
+              Always take control of
+            </Typo>
+            <Typo size={30} fontWeight={"700"}>
+              your finance
+            </Typo>
+          </Animated.View>
+
+          {/* Footer Description */}
+          <Animated.View
+            entering={FadeInDown.duration(1000)
+              .delay(100)
+              .springify()
+              .damping(12)}
+            style={{ alignItems: "center", gap: 2 }}
+          >
+            <Typo size={13} color={colors.textLight}>
+              Stay on top of your budget and make
+            </Typo>
+            <Typo size={13} color={colors.textLight}>
+              informed financial decisions.
+            </Typo>
+          </Animated.View>
+
+          {/* Button */}
+          <Animated.View
+            entering={FadeInDown.duration(1000)
+              .delay(200)
+              .springify()
+              .damping(12)}
+            style={styles.buttonContainer}
+          >
+            <Button style={{ width: verticalScale(350) }}>
+              <Typo size={20} color={colors.neutral900} fontWeight={"700"}>
+                Get Started
+              </Typo>
+            </Button>
+          </Animated.View>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingTop: spacingY._7,
+  },
+  welcomeImage: {
+    width: "100%",
+    height: verticalScale(300),
+    alignSelf: "center",
+    marginTop: verticalScale(100),
+  },
+  loginButton: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+  footer: {
+    backgroundColor: colors.neutral900,
+    alignItems: "center",
+    paddingTop: verticalScale(30),
+    paddingBottom: verticalScale(45),
+    gap: spacingY._20,
+    shadowColor: "white",
+    shadowOffset: {
+      width: 0,
+      height: -10,
+    },
+    elevation: 10,
+    shadowRadius: 20,
+    shadowOpacity: 0.15,
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: spacingX._25,
+  },
+});
